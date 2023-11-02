c0957432419b812716fd16698fe931851272f5cf
Moayad Maher <its.moayadj@gmail.com>
2023-11-02 21:50:46 +0300

    make a reuseable autocomplete widget

--- Movie-fighter/index.js
+++ Movie-fighter/index.js
@@ -1,64 +1,90 @@
-const fetchData = async (searchTerm) => {
-    const { data } = await axios.get('http://www.omdbapi.com', {
+createAtuoComplete({
+    root: document.querySelector('.autocomplete'),
+
+    renderOption(movie) {
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+        return `
+            <img src= "${imgSrc}"/>
+            ${movie.Title} (${movie.Year})
+            `;
+    },
+
+    onOptionSelect(movie) {
+        onMovieSelect(movie)
+    },
+
+    inputValue(movie) {
+        return movie.Title;
+    },
+
+    async fetchData(searchTerm) {
+        const { data } = await axios.get('http://www.omdbapi.com', {
+            params: {
+                apikey: 'c8af1caa',
+                s: searchTerm
+            }
+        });
+
+        if (data.Error) {
+            return []
+        }
+        return data.Search
+    }
+
+
+})
+
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com', {
         params: {
             apikey: 'c8af1caa',
-            s: searchTerm
+            i: movie.imdbID
         }
-    });
+    })
 
-    if (data.Error) {
-        return []
-    }
-    return data.Search
-};
-
-const root = document.querySelector('.autocomplete')
-root.innerHTML = `
-    <label><b>Search For a Movie</b></label>
-    <input class="input"/>
-    <div class="dropdown">
-        <div class="dorpdown-menu">
-            <div class="dropdown-content results">
-            </div>
-        </div>
-    </div>
-`
-
-const input = document.querySelector('input')
-const dropdown = document.querySelector('.dropdown')
-const resultWrapper = document.querySelector('.results')
-
-
-const onInput = async (event) => {
-    const movies = await fetchData(event.target.value);
-    console.log(movies)
-
-    resultWrapper.innerHTML = '';
-    dropdown.classList.add('is-active')
-
-    for (let movie of movies) {
-        const option = document.createElement('a')
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
-
-        option.classList.add('dropdown-item')
-        option.innerHTML = `
-            <img src= "${imgSrc}"/>
-            ${movie.Title}
-            `;
-
-        resultWrapper.appendChild(option);
-    }
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
 }
 
+const movieTemplate = (movieDetail) => {
+    return `
+    <article class="media">
+        <figure class"media-left">
+            <p class="image">
+                <img src="${movieDetail.Poster}"/>
+            </p>
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <h1>${movieDetail.Title}</h1>
+                <h4>${movieDetail.Genre}</h4>
+                <p>${movieDetail.Plot}</p>
+            </div>
+        </div>
+    </article>
 
-input.addEventListener('input', debounce(onInput, 500))
+    <article class ="notification is-primary">
+        <p class="title">${movieDetail.Awards}</p>
+        <p class="subtitle">Awards</p>
+    </article>
 
-// document.addEventListener('click', event => {
-
-//     console.log(event.target)
-
-//     if (!root.contains(event.target)) {
-//         dropdown.classList = ""
-//         console.log(event.target)
-//     }
-// })
+    <article class ="notification is-primary">
+        <p class="title">${movieDetail.BoxOffice}</p>
+        <p class="subtitle">Box Office</p>
+    </article>
+    
+    <article class ="notification is-primary">
+        <p class="title">${movieDetail.Metascore}</p>
+        <p class="subtitle">Metascore</p>
+    </article>
+    
+    <article class ="notification is-primary">
+        <p class="title">${movieDetail.imdbRating}</p>
+        <p class="subtitle">IMDB Rating</p>
+    </article>
+    
+    <article class ="notification is-primary">
+        <p class="title">${movieDetail.imdbVotes}</p>
+        <p class="subtitle">IMDB Votes</p>
+    </article>
+    `;
+};
